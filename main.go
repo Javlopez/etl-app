e61bcb773dc6b8688b4c9d18fbff27e2f0d376b8
Javier Lopez Lopez <sjavierlopez@gmail.com>
2017-10-27 01:38:52 -0500

    Adding performance

--- main.go
+++ main.go
@@ -79,12 +79,22 @@ func transform(extractChannel, transformChannel chan *Order) {
 	}
 
 
-	for o := range extractChannel {
-		time.Sleep(3 * time.Millisecond)
-		o.UnitCost = productList[o.PartNumber].UnitCost
-		o.UnitPrice = productList[o.PartNumber].UnitPrice
+	numberMessages := 0
 
-		transformChannel <- o
+	for o := range extractChannel {
+
+		numberMessages++
+		go func(o *Order) {
+			time.Sleep(3 * time.Millisecond)
+			o.UnitCost = productList[o.PartNumber].UnitCost
+			o.UnitPrice = productList[o.PartNumber].UnitPrice
+			transformChannel <- o
+			numberMessages--
+		}(o)
+	}
+
+	for ;numberMessages > 0; {
+		time.Sleep(1 * time.Millisecond)
 	}
 
 	close(transformChannel)
@@ -100,14 +110,25 @@ func load(transformChannel chan *Order, doneChannel chan bool) {
 		"Unit Cost", "Unit Price",
 		"Total Cost", "Total Price")
 
+	numberMessages := 0
+
 	for  o := range transformChannel{
-		time.Sleep(1 * time.Millisecond)
-		fmt.Fprintf(f, "%20s %15d %12.2f %12.2f %15.2f %15.2f\n",
-			o.PartNumber, o.Quantity,
-			o.UnitCost, o.UnitPrice,
-			o.UnitCost*float64(o.Quantity),
-			o.UnitPrice*float64(o.Quantity))
+		numberMessages++
+		go func(o *Order) {
+			time.Sleep(1 * time.Millisecond)
+			fmt.Fprintf(f, "%20s %15d %12.2f %12.2f %15.2f %15.2f\n",
+				o.PartNumber, o.Quantity,
+				o.UnitCost, o.UnitPrice,
+				o.UnitCost*float64(o.Quantity),
+				o.UnitPrice*float64(o.Quantity))
+			numberMessages--
+		}(o)
 	}
+
+	for ;numberMessages > 0; {
+		time.Sleep(1 * time.Millisecond)
+	}
+
 
 	doneChannel <- true
 }
